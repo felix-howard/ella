394cc71b6d8aea6340d11b1c3b0915d056b6adbc
felix-howard <phuoc.alphamedia@gmail.com>
2026-01-22 23:41:08 +0700

    [Fix] | Use skipNodeModulesBundle to exclude all native deps

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
   dts: true,
+  skipNodeModulesBundle: true,
   noExternal: ['@ella/db', '@ella/shared'],
-  external: ['sharp', 'pdf-poppler'],
 })
